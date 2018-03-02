770ed6e258e4e029f1682327849fda81603a2267
matheussilva97 <matheus.alcantara97@hotmail.com>
2018-03-02 20:40:25 -0300

    implementação para o programa continuar até o usuario informar a opção de sair

--- agenda.py
+++ agenda.py
@@ -1,19 +1,28 @@
-#Agenda Telefonica
+# Agenda Telefonica
+
 import funcoes
 
 funcoes.bemvindo()
 
-#Opcoes do Usuario
-opcao = int(raw_input())
+# Opcoes do Usuario
+opcao = int(input())
 print("Seleccionaste", opcao)
 
 
-#Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-else:
-	funcoes.falha()
+while opcao != 9:
 
+	# Estrutura de controle
+	if opcao == 1:
+		funcoes.adicionar()
+	elif opcao == 2:
+		funcoes.listar()
+	elif opcao == 9:
+		exit()
+	else:
+		funcoes.falha()
 
+	funcoes.bemvindo()
+
+	# Opcoes do Usuario
+	opcao = int(input())
+	print("Seleccionaste", opcao)

39539f3f32e6edbb81eaafb612c77196e0309c68
ybarros7 <36777756+ybarros7@users.noreply.github.com>
2018-03-04 19:57:00 -0300

    Desenvolvimento da função "Deletar".

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,18 @@
+import csv
 #Mensagem de Bem Vindo e Opcoes ao Usuario
 def bemvindo():
 	print("Bem Vindo a Agenda")
 	print("Selecione uma Opcao")
 	print("1  Adicionar um novo contato")
 	print("2  Listar os contatos da agenda")
+	print("5  Remover um contato")
 
 #Funcoes do processo
 def adicionar():
 	print("Adicionar um registro")
 	agenda = open("agendatelefonica.csv",'a')
-	nome = raw_input("Nome do Contato:")
-	telefone = raw_input("Digite o telefone:")
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
 	print("Contato salvo com nome:",nome," e numero",telefone)
 	agenda.write(nome)
 	agenda.write(",")
@@ -18,7 +20,25 @@ def adicionar():
 	agenda.write(",")
 	agenda.write("\n")
 	agenda.close()
-	
+
+def deletar():
+	with open("agendatelefonica.csv") as agenda:
+		reader = csv.reader(agenda, delimiter=',')
+
+		lista = list(reader)
+	listar()
+	nome = str("")
+
+	while nome not in ([row[0] for row in lista]):
+		nome = input("Digite o nome do contato que você deseja deletar: ")
+
+	lista.pop([row[0] for row in lista].index(nome))
+
+	with open("agendatelefonica.csv", "w") as f:
+		writer = csv.writer(f)
+		for row in lista:
+			writer.writerow(row)
+
 def listar():
 	print("Lista de Contatos")
 	agenda = open("agendatelefonica.csv")

bfcea8953d5b2d6fabac90e078bc03ff618d5709
Jefferson Rubio de Oliveira <jerubio@live.com>
2018-03-08 23:29:21 -0300

    Criação do item 4
    Membros Leonardo - RA: 1701080
    Guilherme - RA: 1701804
    Giovanni - 1701785
    Gabriel - 1701170
    Jefferson Rubio - 170072
    Carolina - 1701603
    Reinaldo - 1510402
    Rubiana -

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,23 @@
+import funcoes
+import sys
+import shelve
+
 #Mensagem de Bem Vindo e Opcoes ao Usuario
+
 def bemvindo():
 	print("Bem Vindo a Agenda")
 	print("Selecione uma Opcao")
 	print("1  Adicionar um novo contato")
 	print("2  Listar os contatos da agenda")
+	print("5  Excluir contatos")
+	
 
 #Funcoes do processo
 def adicionar():
 	print("Adicionar um registro")
 	agenda = open("agendatelefonica.csv",'a')
-	nome = raw_input("Nome do Contato:")
-	telefone = raw_input("Digite o telefone:")
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
 	print("Contato salvo com nome:",nome," e numero",telefone)
 	agenda.write(nome)
 	agenda.write(",")
@@ -31,3 +38,26 @@ def listar():
 
 def falha():
 	print("Opcao Incorreta")
+	
+def procura_numero(agenda, nome):
+        agenda = open("agendatelefonica.csv")
+        nome = input()
+        if nome in agenda:
+            print(nome, agenda[nome])
+        else:
+            print(nome, "não está na agenda.")
+        agenda.close()
+
+
+def apaga():
+        print ("Opção exclir contato")
+        agenda = [ line for line in open("agendatelefonica.csv")]
+        nome = input("Nome do Contato:")
+        for item in agenda:
+                if nome in item:
+                        agenda.remove(item)
+                        print(item)
+        print(agenda)
+        archive = open("agendatelefonica.csv", 'w')
+        archive.writelines(agenda)
+        archive.close()

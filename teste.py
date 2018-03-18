d435ca76aab4a3238518d245013af8cc280cac82
guilhermegdf <36552622+guilhermegdf@users.noreply.github.com>
2018-03-18 10:29:44 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,16 @@
 
 import funcoes
 
-x="Renato"
+'''x="Renato"
 listar = funcoes.buscar(x)
 
 assert listar == "Renato"
-print(listar)
+print(listar)'''
+
+y=4
+agenda = funcoes.bemvindo(y)
+assert agenda == funcoes.buscar()
+
+y=9
+agenda = funcoes.bemvindo(y)
+assert agenda == funcoes.ecerramento()

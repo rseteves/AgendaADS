8a63a5e9641a81464ec33404ac73f006b1c2cecf
guilhermegdf <36552622+guilhermegdf@users.noreply.github.com>
2018-03-20 13:32:14 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -58,7 +58,7 @@ def buscar(x):
         lista = []
         lt=[]
         print("Busca de Contatos")
-        with open("/dscard/agendatelefonica.csv") as agenda:
+        with open("agendatelefonica.csv") as agenda:
                 reader = csv.reader(agenda)
                 for linha in reader:
                         lista.append(linha)

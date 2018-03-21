60e73668809e9e4c731b5d53db4ce735a57867f2
GiovanniPC <36246276+GiovanniPC@users.noreply.github.com>
2018-03-21 19:29:52 -0300

    teste

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ import funcoes
 funcoes.adicionar("Gigio","1234")
 
 agenda = open("agendatelefonica.csv",'a')
-if nome in agenda:
-              assert print(nome, agenda[nome])
+if Gigio in agenda:
+              assert print(Gigio, agenda[Gigio])
         

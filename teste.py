7c8fc6b9e2dd580c62bb3f267bad818794bbd7b9
GiovanniPC <36246276+GiovanniPC@users.noreply.github.com>
2018-03-21 19:06:21 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,10 @@
+import funcoes
+
+
+funcoes.adicionar("Gigio",1234)
+
+agenda = open("agendatelefonica.csv",'a')
+if Gigio in agenda:
+              assert print(Gigio, agenda[Gigio]) 
+        agenda.close()
+        

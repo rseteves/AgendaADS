5fad651ebb5a20fb45ae44d204d1a9a7a46e3075
Jefferson Kwak <36552733+jeffersonkr@users.noreply.github.com>
2018-03-23 20:43:52 -0300

    loop para menu e sair apenas com opcao 9

--- agenda.py
+++ agenda.py
@@ -1,25 +1,31 @@
 #Agenda Telefonica
 import funcoes
 
-funcoes.bemvindo()
+x = 0
+while x != 9: 
+	funcoes.bemvindo()
 
-#Opcoes do Usuario
-opcao = int(input())
-print("Seleccionaste", opcao)
-
-
-#Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-elif opcao == 3:
-        funcoes.excluir()
-elif opcao == 4:
-	funcoes.buscar_nome()
-elif opcao == 9:
-        exit()
-else:
-	funcoes.falha()
+	#Opcoes do Usuario
+	opcao = int(input())
+	print("Seleccionaste", opcao)
+	
+	#Estrutura de controle
+	if opcao == 1:
+		funcoes.adicionar()
+		x = 1
+	elif opcao == 2:
+		funcoes.listar()
+		x = 2
+	elif opcao == 3:
+		funcoes.excluir()
+		x = 3
+	elif opcao == 4:
+		funcoes.buscar_nome()
+		x = 4
+	elif opcao == 9:
+		exit()
+		x = 9
+	else:
+		funcoes.falha()
 
 

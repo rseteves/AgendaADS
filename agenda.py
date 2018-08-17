25231315276e9d9d0147362204d5681ebba8a202
João Victor Gonçalves <30908697+JVGoncalves@users.noreply.github.com>
2018-08-16 22:07:42 -0300

    Alteração em agenda e funções

--- agenda.py
+++ agenda.py
@@ -4,16 +4,15 @@ import funcoes
 funcoes.bemvindo()
 
 #Opcoes do Usuario
-opcao = int(raw_input())
-print("Seleccionaste", opcao)
-
+opcao = int(input(""))
+print("Você selecionou", opcao)
 
 #Estrutura de controle
 if opcao == 1:
 	funcoes.adicionar()
 elif opcao == 2:
 	funcoes.listar()
+elif opcao == 3:
+	funcoes.sairDoPrograma()
 else:
 	funcoes.falha()
-
-

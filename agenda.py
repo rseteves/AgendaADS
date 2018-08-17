582ee9bbf0518e3467855be0ce7939bbb34b718c
Vinicius Machado <vinicius.machado@smartconsulting.com.br>
2018-08-17 16:19:05 -0300

    Corrigindo Bug do Raw_Input

--- agenda.py
+++ agenda.py
@@ -4,7 +4,7 @@ import funcoes
 funcoes.bemvindo()
 
 #Opcoes do Usuario
-opcao = int(raw_input())
+opcao = int(input())
 print("Seleccionaste", opcao)
 
 
@@ -12,7 +12,7 @@ print("Seleccionaste", opcao)
 if opcao == 1:
 	funcoes.adicionar()
 elif opcao == 2:
-	funcoes.listar()
+        funcoes.listar()
 else:
 	funcoes.falha()
 

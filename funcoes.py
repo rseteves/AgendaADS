32ed1ee2ff8ae8409117a692b41da7ad030c374c
João Victor Gonçalves <30908697+JVGoncalves@users.noreply.github.com>
2018-08-16 22:02:14 -0300

    Ajustes em Agenda e Funções

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,19 @@
+import sys
+
 #Mensagem de Bem Vindo e Opcoes ao Usuario
 def bemvindo():
 	print("Bem Vindo a Agenda")
-	print("Selecione uma Opcao")
-	print("1  Adicionar um novo contato")
-	print("2  Listar os contatos da agenda")
+	print("\nSelecione uma opção:")
+	print("1 -> Adicionar um novo contato")
+	print("2 -> Listar os contatos da agenda")
+	print("3 -> Sair do programa")
 
 #Funcoes do processo
 def adicionar():
 	print("Adicionar um registro")
 	agenda = open("agendatelefonica.csv",'a')
-	nome = raw_input("Nome do Contato:")
-	telefone = raw_input("Digite o telefone:")
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
 	print("Contato salvo com nome:",nome," e numero",telefone)
 	agenda.write(nome)
 	agenda.write(",")
@@ -31,3 +34,7 @@ def listar():
 
 def falha():
 	print("Opcao Incorreta")
+
+def sairDoPrograma():
+	print("\nA aplicação foi encerrada!")
+	sys.exit()

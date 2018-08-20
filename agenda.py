39c7a3e13e0bb6e5385f7c6be396e542e58cb454
Queiroz21 <39350802+Queiroz21@users.noreply.github.com>
2018-08-20 09:36:04 -0300

    Ajuste na estrutura de controle
    Inserção das novas funções

--- agenda.py
+++ agenda.py
@@ -13,6 +13,12 @@ if opcao == 1:
 	funcoes.adicionar()
 elif opcao == 2:
 	funcoes.listar()
+elif opcao == 3:
+	funcoes.Buscar()
+elif opcao == 2:
+	funcoes.DeletarContato()
+elif opcao == 2:
+	funcoes.sairDoPrograma()
 else:
 	funcoes.falha()
 

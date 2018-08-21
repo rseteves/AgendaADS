6616b581f269794400dba826ec1f4ea07c44ba80
Queiroz21 <39350802+Queiroz21@users.noreply.github.com>
2018-08-21 09:27:28 -0300

    Inserção de método de controle
    Adicionada função de controle no Arquivo funções, e remoção dos if e else do arquivo agenda, tornando tudo aquilo uma função

--- agenda.py
+++ agenda.py
@@ -1,19 +1,7 @@
 #Agenda Telefonica
 import funcoes
 
+#Realiza o inicio do programa mostrando as opções da Agenda ao usuário
 funcoes.bemvindo()
 
-#Opcoes do Usuario
-opcao = int(input())
-print("Seleccionaste", opcao)
 
-
-#Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-elif opcao == 3:
-	funcoes.busca()
-else:
-	funcoes.falha()

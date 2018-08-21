5bc4d6f71069df482df7b4d6a0cf1913c0f6a4ba
Diego Saraiva <38359555+diegoalmeidas@users.noreply.github.com>
2018-08-21 18:54:24 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -4,31 +4,82 @@
 #Rogerio Alves Santos		RA1800386
 #William Bruce Ogura		RA1800114
 
-#Agenda Telefonica
+import csv
 
-import funcoes
+#Mensagem de Bem Vindo e Opcoes ao Usuario
+def bemvindo():
+	print("Bem Vindo a Agenda")
+	print("Selecione uma Opcao")
+	print("1  Adicionar um novo contato")
+	print("2  Listar os contatos da agenda")
+	print("3  Sair do programa")
+	print("4  Apagar um contato")
+	print("5  Buscar um contato")
 
-funcoes.bemvindo()
+  
+#Funcoes do processo
+def adicionar():
+	print("Adicionar um registro")
+	agenda = open("agendatelefonica.csv",'a')
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
+	print("Contato salvo com nome:",nome," e numero",telefone)
+	agenda.write(nome)
+	agenda.write(",")
+	agenda.write(telefone)
+	agenda.write(",")
+	agenda.write("\n")
+	agenda.close()
+	
+# Listar linhas da agenda
+def numlinhas():
+	arquivo = open("agendatelefonica.csv", "r")
+	n_linhas = sum(1 for linha in arquivo)
+	arquivo.close()
+	return n_linhas
 
-#Opcoes do Usuario
-opcao = int(input("Digite uma opção:"))
-print("Selecionaste", opcao)
+def listar():
+	qtdlinhas = numlinhas()
+	print("Lista de Contatos")
+	agenda = open("agendatelefonica.csv")
+	numero = 0
+	while numero < qtdlinhas:
+		print (agenda.readline())
+		numero = numero + 1
+	print("Listado correctamente")	
+	agenda.close()
+
+def sair():
+	print("########## Saindo do programa ##########")
+	import sys
+	sys.exit()
 
 
-#Estrutura de controle
-while opcao != 0:
-        if opcao == 1:
-        	funcoes.adicionar()
-        elif opcao == 2:
-                funcoes.listar()
-        elif opcao == 3:
-                funcoes.sair()
-        elif opcao == 4:
-                funcoes.deletar()
-        elif opcao == 5:
-        	funcoes.encontrar(str(input("Digite o nome que deseja encontrar:")))
-        elif opcao != 1 or opcao != 2 or opcao != 3 or opcao != 4 or opcao != 5:
-                funcoes.falha()
-        funcoes.bemvindo()
-        opcao = int(input("Digite uma opção:"))
-        print("Selecionaste: >>>>>>>>", opcao)
+
+def deletar():
+        with open("agendatelefonica.csv","r") as agenda:
+                reader = csv.reader(agenda)
+                data = list(reader)
+        nome=str("")
+        while nome not in ([row[0] for row in data]):
+                nome = input("Nome do contato a ser deletado: ")
+        data.pop([row[0] for row in data].index(nome))
+        with open("agendatelefonica.csv","w",newline="") as file:
+                writer = csv.writer(file)
+                for row in data :
+                        writer.writerow(row)
+        print("Contato removido com sucesso!!")
+
+def encontrar(busca):
+    agenda = open("agendatelefonica.csv")
+    lista = (agenda.readlines())
+    nome = False
+    for i in range (0,len(lista)):
+        if busca in lista[i]:
+            print(lista[i])
+            nome = True
+    if nome == False:
+            print("Nome não encontrado")
+
+def falha():
+	print("Opcao Incorreta")

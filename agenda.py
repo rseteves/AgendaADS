6eac6e3e6038662de38e9165fad86fac9bfb4c3e
Diego Saraiva <38359555+diegoalmeidas@users.noreply.github.com>
2018-08-21 18:53:37 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -5,23 +5,30 @@
 #William Bruce Ogura		RA1800114
 
 #Agenda Telefonica
+
 import funcoes
 
 funcoes.bemvindo()
 
 #Opcoes do Usuario
-opcao = int(input())
-print("Seleccionaste", opcao)
+opcao = int(input("Digite uma opção:"))
+print("Selecionaste", opcao)
 
 
 #Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-elif opcao == 4:
-	funcoes.deletar()
-elif opcao == 5:
-        funcoes.encontrar(str(input("Digite o nome que deseja encontrar:")))
-else:
-	funcoes.falha()	
+while opcao != 0:
+        if opcao == 1:
+        	funcoes.adicionar()
+        elif opcao == 2:
+                funcoes.listar()
+        elif opcao == 3:
+                funcoes.sair()
+        elif opcao == 4:
+                funcoes.deletar()
+        elif opcao == 5:
+        	funcoes.encontrar(str(input("Digite o nome que deseja encontrar:")))
+        elif opcao != 1 or opcao != 2 or opcao != 3 or opcao != 4 or opcao != 5:
+                funcoes.falha()
+        funcoes.bemvindo()
+        opcao = int(input("Digite uma opção:"))
+        print("Selecionaste: >>>>>>>>", opcao)

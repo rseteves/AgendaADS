e476c90d9a6a742eb993d2312d88bf7876a6cf5c
ClarissaLeme <42013716+ClarissaLeme@users.noreply.github.com>
2018-08-22 20:50:26 -0300

    Adicionado a função de deletar contato.

--- agenda.py
+++ agenda.py
@@ -3,22 +3,6 @@ import funcoes
 
 funcoes.bemvindo()
 #Opcoes do Usuario
-opcao = int(input())
-print("Selecionaste", opcao)
 
-
-#Estrutura de controle
-if opcao==1:
-	funcoes.adicionar()
-elif opcao==2:
-        funcoes.listar()
-elif opcao==3:
-        funcoes.deletanum()
-elif opcao==4:
-        funcoes.Busca()
-elif opcao==5:
-        funcoes.Sair()
-else:
-	funcoes.falha()
 
 

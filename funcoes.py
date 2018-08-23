148419b1cb7dc83448039c079d3390bcae05af7d
philipesan <philipesan123@hotmail.com>
2018-08-23 19:56:26 -0300

    Adicionada a funcao de remover arquivo

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-#Mensagem de Bem Vindo e Opcoes ao Usuario
+﻿#Mensagem de Bem Vindo e Opcoes ao Usuario
 def bemvindo():
 	print("""Selecione a Opção: 
 			1 - Adicionar 	
@@ -33,3 +33,23 @@ def listar():
 
 def falha():
 	print("Opcao Incorreta")
+
+def deletar(var_nomedeletar)
+    print(("""Tem certeza que quer deletar essa entrada? 
+    
+    1 - Sim
+    2 - Não
+    """))
+    vopcao = (int(input("Opção: "))
+    if vopcao == 1:
+        text_file = open("agendatelefonica.csv", "r") ##Abre o Arquivo
+        nomes = text_file.readlines() ##Le todas as linhas 
+        text_file.close() ##Fecha o arquivo
+        text_file = open("agendatelefonica.csv", "w") ##Abre o Arquivo para escrita
+        for linha in nomes:  ##Percorre o Arquivo
+            if linha != var_nomedeletar + '\n':  ##Pergunta se a linha é a que queremos deletar
+                text_file.write(linha)  ##Se não for, reescreve a linha no arquivo
+        text_file.close()  ##Fecha o Arquivo
+        print("Registro deletado com sucesso")  ##Feedback
+    else:
+        print("Registro preservado")

d13dc934b0dc1180f64bbb0b1a27887fe685b53f
Guilherme Vinícius Pignatari <37876043+GuilhermePignatari@users.noreply.github.com>
2018-08-22 22:17:53 -0300

    Sorry2

--- funcoes.py
+++ funcoes.py
@@ -10,7 +10,7 @@ def adicionar():
 	print("Adicionar um registro")
 	agenda = open("agendatelefonica.csv",'a')
 	nome = raw_input("Nome do Contato:")
-	telefone = input("Digite o telefone:")
+	telefone = raw_input("Digite o telefone:")
 	print("Contato salvo com nome:",nome," e numero",telefone)
 	agenda.write(nome)
 	agenda.write(",")

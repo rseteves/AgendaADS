2f32ec702a8d6fb8f5214f547f3ab897c2b93e57
becristinadg <40190781+becristinadg@users.noreply.github.com>
2018-08-23 18:35:18 -0300

    Inserção da função buscar
    Inserção da função buscar e ajuste da função listar

--- funcoes.py
+++ funcoes.py
@@ -1,33 +1,45 @@
+import csv
 #Mensagem de Bem Vindo e Opcoes ao Usuario
 def bemvindo():
 	print("Bem Vindo a Agenda")
 	print("Selecione uma Opcao")
-	print("1  Adicionar um novo contato")
-	print("2  Listar os contatos da agenda")
+	print("1 Adicionar um novo contato")
+	print("2 Listar os contatos da agenda")
+	print("3 Buscar")
+	print("4 Deletar")
+	print("5 Sair do programa")
 
 #Funcoes do processo
+def opcao():
+        x = int(input("Insira uma opção: "))
+        return x
+
+def buscar():
+        agenda = csv.reader(open('agendatelefonica.csv', 'r'))
+        nome = input("Digite o nome a ser procurado: ")
+        for linhas in agenda:
+                if linhas[0] == nome:
+                        print(linhas)
+
 def adicionar():
 	print("Adicionar um registro")
 	agenda = open("agendatelefonica.csv",'a')
-	nome = raw_input("Nome do Contato:")
-	telefone = raw_input("Digite o telefone:")
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
 	print("Contato salvo com nome:",nome," e numero",telefone)
 	agenda.write(nome)
 	agenda.write(",")
 	agenda.write(telefone)
-	agenda.write(",")
 	agenda.write("\n")
 	agenda.close()
 	
 def listar():
-	print("Lista de Contatos")
-	agenda = open("agendatelefonica.csv")
-	numero = 0
-	while numero < 25:
-		print (agenda.readline())
-		numero = numero + 1
-	print("Listado correctamente")	
-	agenda.close()
+        print("Lista de Contatos")
+        agenda = open("agendatelefonica.csv")
+        for linhas in agenda:
+                print(linhas)
+        print("Listado corretamente")	
+        agenda.close()
 
 def falha():
 	print("Opcao Incorreta")

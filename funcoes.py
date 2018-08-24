320fc3f5089811fa640d28c420ef3a04c458aa85
Matheus Silva da Cruz <matheusxx20111@hotmail.com>
2018-08-23 21:35:23 -0300

    Ajuste Função Remover

--- funcoes.py
+++ funcoes.py
@@ -3,9 +3,9 @@ def bemvindo():
 	print("""Selecione a Opção: 
 			1 - Adicionar 	
 			2 - Listar 
-			3 - Buscar 
-			4 - Sair""")
-	opcao = int(input("Opção: "))
+			3 - Buscar
+			4 - Remover 
+			5 - Sair""")
 
 #Funcoes do processo
 def adicionar():
@@ -54,22 +54,24 @@ def buscar():
 			resultados = resultados.split(",")
 			print("Nome:",resultados[0]," - Contato:",resultados[1])
 
-def deletar(var_nomedeletar)
-    print(("""Tem certeza que quer deletar essa entrada? 
-    
+def deletar():
+	lista_arquivo = []
+	var_nomedeletar = input("Digite o nome do contato para deletar:")
+	print(("""Tem certeza que quer deletar essa entrada? 
     1 - Sim
     2 - Não
     """))
-    vopcao = (int(input("Opção: "))
-    if vopcao == 1:
-        text_file = open("agendatelefonica.csv", "r") ##Abre o Arquivo
-        nomes = text_file.readlines() ##Le todas as linhas 
-        text_file.close() ##Fecha o arquivo
-        text_file = open("agendatelefonica.csv", "w") ##Abre o Arquivo para escrita
-        for linha in nomes:  ##Percorre o Arquivo
-            if linha != var_nomedeletar + '\n':  ##Pergunta se a linha é a que queremos deletar
-                text_file.write(linha)  ##Se não for, reescreve a linha no arquivo
-        text_file.close()  ##Fecha o Arquivo
-        print("Registro deletado com sucesso")  ##Feedback
-    else:
-        print("Registro preservado")
+	vopcao = int(input("Opção: "))
+	if vopcao == 1:
+		text_file = open("agendatelefonica.csv", "r") ##Abre o Arquivo
+		nomes = text_file.readlines() ##Le todas as linhas 
+		text_file.close() ##Fecha o arquivo
+		text_file = open("agendatelefonica.csv", "w") ##Abre o Arquivo para escrita
+		for linha in nomes:  ##Percorre o Arquivo
+			lista_arquivo = linha.split(",")
+			if lista_arquivo[0] != var_nomedeletar:  ##Pergunta se a linha é a que queremos deletar
+				text_file.write(linha)  ##Se não for, reescreve a linha no arquivo
+		text_file.close()  ##Fecha o Arquivo
+		print("Registro deletado com sucesso")  ##Feedback
+	else:
+		print("Registro preservado")

dc517a47bfafaa42853bca07a525acb772d1488c
maiyuri <42254309+maiyuri@users.noreply.github.com>
2018-09-02 21:20:39 -0300

    Corrigindo a função bem vindo, adicionar e listar.

--- funcoes.py
+++ funcoes.py
@@ -1,56 +1,43 @@
-#Mensagem de Bem Vindo e Opcoes ao Usuario
+import csv
+
+
 def bemvindo():
 	print("Bem Vindo a Agenda")
-	print("Selecione uma Opcao")
+	print("\n")
+	print("Opções disponíveis: ")
 	print("1  Adicionar um novo contato")
 	print("2  Listar os contatos da agenda")
+	print("3  Apagar um contato")
+	print("4  Buscar um contato")
+	print("5  Encerrar o programa")
+	print("\n")
 
-
+  
+#Funcoes do processo
 def adicionar():
-	print("Adicionar um registro")
+	print("Adicionar um novo contato")
+	print("\n")
 	agenda = open("agendatelefonica.csv",'a')
-	nome = raw_input("Nome do Contato:")
-	telefone = raw_input("Digite o telefone:")
-	print("Contato salvo com nome:",nome," e numero",telefone)
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
+	print("\n")
+	print("Contato salvo com Nome:",nome," e Número:",telefone)
 	agenda.write(nome)
 	agenda.write(",")
 	agenda.write(telefone)
 	agenda.write(",")
 	agenda.write("\n")
 	agenda.close()
+	print("\n")
 	
-def listar():
-	print("Lista de Contatos")
+
+def listarR():
+	print("Lista de Contatos:")
 	agenda = open("agendatelefonica.csv")
-	numero = 0
-	while numero < 25:
-		print (agenda.readline())
-		numero = numero + 1
-	print("Listado correctamente")	
+	contact = csv.reader(agenda)
+	for y in contact:
+		print (y)
+	print("\n")
+	print("Contatos listados corretamente!")
+	print("\n")
 	agenda.close()
-
-def falha():
-	print("Opcao Incorreta")
-
-def buscar():
-	arquivo = csv.reader(open('agenndatelefonica.csv', 'r'))
-	nome = input('Digite o nome procurado: ")
-	for rows in arquivo:
-		     if rows[0] == nome
-		     print("Contato buscado: ", rows)
-    back()
-
-	
-def apagar():
-    agenda = [line for line in open("agendatelefonica.csv")]
-    nomeApagar = input("Digite o nome do contato a ser deletado: ").lower().capitalize()
-    for y in agenda:
-        if nomeApagar in y:
-            agenda.remove(y)
-
-    
-    OutraAgenda= open("agendatelefonica.csv", 'w')
-    OutraAgenda.writelines(agenda)
-    OutraAgenda.close()
-
-    listar()
